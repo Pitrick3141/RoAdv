14a072a70b747e2911c2964f1d15b97c48b2ca68
Jrffy666 <97010985+Jrffy666@users.noreply.github.com>
2022-08-14 01:09:10 -0400

    Undo comment forms

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     # 初始化全局变量
     Globles.init()
     # 初始化开始页面
-    # FormHome.init()
+    FormHome.init()
     # 显示开始页面
-    # FormHome.display()
+    FormHome.display()
     # 初始化开始页面
     FormReady.init()
     # 显示开始页面
